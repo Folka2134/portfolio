7aab4eb211f6703fde8925152ed8e949879b4ef5
folka2134 <folka2134@gmail.com>
2021-10-26 22:18:21 +0100

    Implemented React-scroll to cover button

--- src/components/cover/cover.component.jsx
+++ src/components/cover/cover.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+// import * as Scroll from "react-scroll"
+import { Link } from 'react-scroll';
 
 import { FaFolder } from 'react-icons/fa'
 
 import './cover.styles.css'
 
 const Cover = () => {
+
+  
+
+
   return (
     <div className="cover-container h-screen bg-green-900 flex justify-center items-center">
       <div className="inner-container mt-10 w-96 h-96 flex justify-center items-center text-white">
@@ -13,7 +19,7 @@ const Cover = () => {
           <hr className='' />
           <p className='justify-self-center mt-7'>Otis Peters-Murphy</p>
           <div className='justify-self-center mt-5'>
-            <a href="#banner" className="button rounded-large p-9"><FaFolder size={25}/></a>
+            <Link to="banner-container" smooth={true} className="button rounded-large p-9"><FaFolder size={25}/></Link>
           </div>
         </header>
       </div>
