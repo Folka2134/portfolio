87c8de592489c1a7cd2e876694f9a811be5bd2c9
folka2134 <folka2134@gmail.com>
2021-10-26 14:57:24 +0100

    Updated reel and cover buttons to be responsive

--- src/components/banner/banner.component.jsx
+++ src/components/banner/banner.component.jsx
@@ -6,17 +6,17 @@ import {SiPostgresql, SiRedux} from "react-icons/si"
 
 const Banner = () => {
   return (
-    <div id="banner" className="banner-container text-center h-4/5 py-24 text-3xl" >
+    <div id="banner" className="banner-container text-center h-4/5 py-24 text-2xl md:text-3xl" >
       <p>Hey my name is Otis and I'm a software developer <br/>based in Brighton, uk </p>
-      <footer className="mt-10 text-base text-white">
+      <footer className="mt-10 text-base grid sm:flex justify-center text-white">
         <a href="#." target="_blank" rel="noreferrer" className="button">Resume</a>
         <a href="https://uk.linkedin.com/in/otis-peters-murphy-0246941a9?trk=people-guest_people_search-card" target="_blank" rel="noreferrer" className="button">Linkedin</a>
         <a href="https://github.com/Folka2134" target="_blank" rel="noreferrer" className="button">Github</a>
       </footer>
       
       <IconContext.Provider value={{color: "#5B5B5B", size:"250"}}>
-        <div className="bg-gray-200 mt-24 overflow-hidden">
-          <div className="flex justify-around w-full">
+        <div className="bg-gray-200 mt-24 p-9 overflow-hidden">
+          <div className="flex flex-wrap justify-around w-full">
             <div className="image"><FaHtml5 /></div>
             <div className="image"><FaJsSquare /></div>
             <div className="image"><FaReact  /></div>
