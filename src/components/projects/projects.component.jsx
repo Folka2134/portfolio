66679ca1cb29e7f5e0ab60bf210cc56d12b186fd
folka2134 <folka2134@gmail.com>
2021-11-09 12:58:10 +0000

    updated project 1

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
 
+import smartBrainPreview from "../../images/smartbrain_preview2.png"
+
 const Projects = () => {
   return (
     <div className="projects-container  bg-dark-white h-auto pb-10">
       <h1 className="mb-8 text-4xl font-medium text-center">Projects</h1>
       <div className="project-list grid grid-cols-1 md:grid-cols-3">
+        <div className="rounded overflow-hidden shadow-md hover:shadow-2xl mx-5 mb-8 transition-all duration-300 ease-linear">
+          <img className="w-full" src={smartBrainPreview} alt="Mountain" />
+          <div className="px-6 py-4 flex">
+            <div className="flex-1 border-r-4">
+              <header className="font-bold text-xl mb-2">Face Detection App</header>
+              <p className="text-gray-700 text-lg">
+              In this App will "detect" a face within an image url and also update a signed in user's "rank".<br></br>
+              The detection is performed by pulling data from an api (clarifai) and the users are stored within my own database.
+              </p>
+            </div>
+            <div className="flex-1 pl-6">
+              <header className="font-bold text-xl mb-2">Project Improvements</header>
+              <ul className="text-gray-700 text-lg list-disc">
+                <li>Implement multi-face detection</li>
+                <li>Implement user score board feature</li>
+                <li>Adjust design for mobile</li>
+              </ul>
+            </div>
+          </div>
+          <div className="px-6 text-center text-white">
+            <span className="tag">React</span>
+            <span className="tag">NodeJs</span>
+            <span className="tag">Postgresql</span>
+          </div>
+          <div className="px-6 pb-2 text-center text-white">
+            <a className="inline-block px-3 py-1 text-sm font-semibold mr-2 mb-2 button">Live</a>
+            <a href="https://github.com/Folka2134/face-detection-app" className="inline-block px-3 py-1 text-sm font-semibold mr-2 mb-2 button" target="_blank">Github</a>
+          </div>
+        </div>
+        <div className="rounded overflow-hidden shadow-md hover:shadow-2xl mx-5 mb-8 transition-all duration-300 ease-linear">
+          <img className="w-full" src="https://images.squarespace-cdn.com/content/v1/55cfb535e4b061baebe310df/1587472623697-IC8B3J3ZIZI0AMIE7U8G/sky+photo+for+website+home+page.jpeg?format=2500w" alt="Mountain" />
+          <div className="px-6 py-4 flex">
+            <div className="flex-1 border-r-4">
+              <header className="font-bold text-xl mb-2">Face Detection App</header>
+              <p className="text-gray-700 text-lg">
+                In this App will "detect" a face within an image url and also update a signed in user's "rank".
+              </p>
+            </div>
+            <div className="flex-1 pl-6">
+              <header className="font-bold text-xl mb-2">Project Improvements</header>
+              <ul className="text-gray-700 text-lg list-disc">
+                <li>Implement multi-face detection</li>
+                <li>Implement user score board feature</li>
+                <li>Adjust design for mobile</li>
+              </ul>
+            </div>
+          </div>
+          <div className="px-6 text-center text-white">
+            <span className="tag">React</span>
+            <span className="tag">NodeJs</span>
+            <span className="tag">Postgresql</span>
+          </div>
+          <div className="px-6 pb-2 text-center text-white">
+            <span className="inline-block px-3 py-1 text-sm font-semibold mr-2 mb-2 button">Live</span>
+            <span className="inline-block px-3 py-1 text-sm font-semibold mr-2 mb-2 button">Github</span>
+          </div>
+        </div>
         <div className="rounded overflow-hidden shadow-md hover:shadow-2xl mx-5 mb-8 transition-all duration-300 ease-linear">
           <img className="w-full" src="https://images.squarespace-cdn.com/content/v1/55cfb535e4b061baebe310df/1587472623697-IC8B3J3ZIZI0AMIE7U8G/sky+photo+for+website+home+page.jpeg?format=2500w" alt="Mountain" />
           <div className="px-6 py-4 flex">
