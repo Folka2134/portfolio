94cbcbeeda1c26a0164ccf8c0acee7c89168bbec
folka2134 <folka2134@gmail.com>
2022-01-17 12:53:39 +0000

    Added phone number to footer and updated styling

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer-container h-16 align-middle text-center font-bold text-white bg-space-background bg-bottom bg-cover bg-fixed">
       <footer >
         <h3>Get in touch!</h3>
-        <p>folka2134@gmail.com</p>
+        <p><span className='bg-tag-black bg-opacity-30 rounded-md'> folka2134@gmail.com || 07712715874</span></p>
       </footer>
     </div>
   );
